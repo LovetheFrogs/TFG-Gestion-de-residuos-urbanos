b7d9da5eb404a1d6d71e53674145f8d4f5914a87
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2025-03-19 21:49:58 +0100

    Style: Fixed docstring

--- Algorithm/AlgoCode/utils/utils.py
+++ Algorithm/AlgoCode/utils/utils.py
@@ -11,21 +11,21 @@ def printProgressBar(iteration: int,
                      printEnd: str = "\r"):
     """Call in a loop to create terminal progress bar
         
-        Thanks to StackOverflow's Greenstick user for this function, extracted
-        from an answer to a thread about progressbars in python.
+    Thanks to StackOverflow's Greenstick user for this function, extracted
+    from an answer to a thread about progressbars in python.
 
-        Args:
-            iteration: Current iteration.
-            total: Total iterations.
-            prefix (optional): Prefix string. Defaults to ''.
-            suffix (optional): Suffix string. Defaults to ''.
-            decimals (optional): positive number of decimals in percent 
-                complete. Defaults to 1.
-            length (optional): character length of bar. Defaults to 100.
-            fill (optional): bar fill character. Defaults to '█'.
-            printEnd (optional): end character (e.g. "\r", "\r\n").
-                Defaults to "\r".
-        """
+    Args:
+        iteration: Current iteration.
+        total: Total iterations.
+        prefix (optional): Prefix string. Defaults to ''.
+        suffix (optional): Suffix string. Defaults to ''.
+        decimals (optional): positive number of decimals in percent 
+            complete. Defaults to 1.
+        length (optional): character length of bar. Defaults to 100.
+        fill (optional): bar fill character. Defaults to '█'.
+        printEnd (optional): end character (e.g. "\r", "\r\n").
+            Defaults to "\r".
+    """
     percent = ("{0:." + str(decimals) + "f}").format(100 *
                                                      (iteration / float(total)))
     filledLength = int(length * iteration // total)
