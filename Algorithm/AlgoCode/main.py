89b8f136ece18fadc5baf612df184ea4f6dae6bb
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2025-03-28 20:17:24 +0100

    Feat: Recoded main module making use of new functions

--- Algorithm/AlgoCode/main.py
+++ Algorithm/AlgoCode/main.py
@@ -10,11 +10,10 @@ def run():
     g = model.Graph()
     g.populate_from_file(f"{cwd}/tests/files/test3.txt", verbose=True)
 
-    # Divide the graph into zones of 1.500kg each.
-    zones = g.divide_graph(1500)
-
-    # Create a subgraph for each zone created.
-    subgraphs = [g.create_subgraph(z) for z in zones]
+    # Divide the graph into zones of 1.500kg each and create the needed 
+    # subgraphs.
+    algo = algorithms.Algorithms(g)
+    subgraphs, zones = algo.divide(1500)
 
     print(f"Number of zones: {len(zones)}")
     print()
@@ -22,6 +21,7 @@ def run():
     total = 0
     # For each subgraph, calculate the lower bound, create a NN tour and use it
     # to find a tour using Tabu-Search. Save it to a file called `subgraph{i}`
+    points = []
     for i, sg in enumerate(subgraphs):
         algo = algorithms.Algorithms(sg)
         print(f"Subgraph {i + 1} - Weight {sg.total_weight()}")
@@ -33,7 +33,7 @@ def run():
         print(f"|   Nearest Neighbor tour value = {nnv:.2f} "
               f"(Within {abs(100 - ((100 * nnv) / lb)):.1f}% "
               f"of the lower bound)")
-        _, tsv = algo.run_tabu_search(path=nnp, 
+        tsp, tsv = algo.run_tabu_search(path=nnp, 
                                       dir=f"{cwd}/problem/plots", 
                                       name=f"subgraph{i + 1}")
         print(f"|   Tabu search tour value = {tsv:.2f} "
@@ -42,8 +42,15 @@ def run():
         print("--------------------------------------------------------------")
         print()
         total += tsv
+        
+        # Save coordinates of the points that form the path.
+        points.append([sg.get_node(n).coordinates for n in tsp])
 
     print(f"Total cost = {total}")
+    
+    # Print all paths calculated
+    algo.plot_all_paths(points, dir=f"{cwd}/problem/plots", name="AllPaths")
+    
 
 
 def main():
