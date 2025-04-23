b3b427c0752e3303adfc1b1379fbc193cdcd141a
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2025-04-23 19:13:18 +0200

    Style: Improved output for benchmarking script

--- Algorithm/AlgoCode/benchmark/run_benchmark.sh
+++ Algorithm/AlgoCode/benchmark/run_benchmark.sh
@@ -1,5 +1,8 @@
 # Runs two benchmarks, one for 100 graphs between 40 and 120 nodes and another 
 # one using TSPLib's graphs.
 
+printf "Running TSPLib benchmark.\n"
 python3 benchmark/benchmark.py -v 1 -m 1
+
+printf "\nRunning randomized benchmark.\n"
 python3 benchmark/benchmark.py -v 1 -m 0
