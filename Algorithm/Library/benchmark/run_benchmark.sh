d3e515c773593b2c6b26150481df157b080bf12b
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2025-07-16 18:41:15 +0200

    Feat: Updated experiment launching script

--- Algorithm/Library/benchmark/run_benchmark.sh
+++ Algorithm/Library/benchmark/run_benchmark.sh
@@ -1,12 +1,6 @@
-# Runs two benchmarks, one for 100 graphs between 40 and 120 nodes and another 
-# one using TSPLib's graphs.
+printf "\nRunning benchmark on varying graph sizes.\n"
+python3 -m Algorithm.Library.benchmark.benchmark_zones_gsize
 
-# printf "Running TSPLib benchmark.\n"
-# python3 benchmark/benchmark.py -v 1 -m 1
-
-printf "\nRunning randomized benchmark on small graphs.\n"
-python3 benchmark/benchmark.py -v 1 -m 0 -i 20 -j 70
-
-printf "\nRunning randomized benchmark on small graphs.\n"
-python3 benchmark/benchmark.py -v 1 -m 0 -i 71 -j 120
+printf "\nRunning benchmark on varying capacities.\n"
+python3 -m Algorithm.Library.benchmark.benchmark_zones_tcap
 
